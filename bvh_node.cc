f9f3024212830fa38192448fc1e957e2dad52cab
Alexander Szczepanski <vampyr@beinacht.org>
2013-12-15 18:27:04 -0800

    split strategie

--- bvh_node.cc
+++ bvh_node.cc
@@ -1,7 +1,6 @@
 #include <limits>
 #include <stdexcept>
 //test
-#include <limits>
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
@@ -30,17 +29,32 @@ void BVHNode::insert(Mesh const& mesh, std::vector<unsigned int>* faceIDs)
 	{
 		triangles.push_back(Triangle(&mesh, var));
 
-		for (int i = 0; i<3; i++)
+		for (int i = 0; i < 3; i++)
 		{
 			if (min[i]>triangles.back().getAABBMin()[i])
-			{
 				min[i] = triangles.back().getAABBMin()[i];
-			}
 			if (max[i]<triangles.back().getAABBMax()[i])
-			{
 				max[i] = triangles.back().getAABBMax()[i];
-			}
 		}
+	}
+
+	std::vector<Triangle> left;
+	std::vector<Triangle> right;
+
+	Vec3f middle = max;
+	int axis = aabb.getLongestAxis();
+	middle[axis] = (min[axis] + max[axis]) / 2;
+
+	AABB half = AABB(min, middle);
+
+	while (triangles.size() > 0)
+	{
+		if (half.inside(triangles.back().getCentroid()))
+			left.push_back(triangles.back());
+		else
+			left.push_back(triangles.back());
+
+		triangles.pop_back();
 	}
 
     this->aabb = AABB(min, max);
